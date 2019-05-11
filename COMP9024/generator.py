127135caa344c09d159285f7d5850aaf2b9fb265
Michael Dajun Feng <50504300+DajunFeng@users.noreply.github.com>
2019-05-11 16:28:18 +1000

    Update generator.py

--- COMP9024/generator.py
+++ COMP9024/generator.py
@@ -9,3 +9,8 @@ def factor(n):
         k += 1
     if k * k == n:
         yield k
+"""
+This function is an example of defining a python Generator
+1. data type of generator: <Class 'generator'>
+2. generator can be iterated to obtain all the elements in it
+"""
