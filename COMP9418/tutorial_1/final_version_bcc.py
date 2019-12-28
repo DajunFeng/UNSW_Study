53ef08f3bab79917a7225f984cd1c5bd06082099
Michael Dajun Feng <50504300+DajunFeng@users.noreply.github.com>
2019-12-28 19:21:37 +1100

    Add files via upload

---
+++ COMP9418/tutorial_1/final_version_bcc.py
@@ -0,0 +1,40 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Michael
+#
+# Created:     28/12/2019
+# Copyright:   (c) Michael 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+# load the dataset
+data = load_breast_cancer()
+
+# organise the data
+label_names = data['target_names']
+labels = data['target']
+feature_names = data['feature_names']
+features = data['data']
+
+# split the data into training and testing
+train, test, training_labels, testing_labels = train_test_split(features, labels,
+test_size=0.33, random_state=42)
+
+# construct the model
+gnb = GaussianNB()
+model = gnb.fit(train, training_labels)
+
+# make prediction
+pred = model.predict(test)
+
+# evaluate the accuracy
+acc = accuracy_score(testing_labels, pred)
+
+print(acc)
